89568de0b1fe7ccbacb3b998b7e4deb6a45325d2
ofarukbulut <96415105+ofarukbulut@users.noreply.github.com>
2025-07-15 15:25:36 +0300

    arimetik işlem önceliği eklendi

---
+++ src/main/java/bulut/com/ArithmeticOperations.java
@@ -0,0 +1,27 @@
+package bulut.com;
+
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = scanner(1);
+        int b = scanner(2);
+        int c = scanner(3);
+        int value = a+b*c-b;
+        System.out.print("Sonuç : " + value);
+    }
+
+    public static int scanner(int i){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.print("Lütfen " + i +". sayısı giriniz!");
+            try {
+                int value = scanner.nextInt();
+                return value;
+            }catch (Exception e){
+                scanner.nextLine();
+                System.out.print("Lütfen sayı giriniz !!!");
+            }
+        }
+    }
+}
