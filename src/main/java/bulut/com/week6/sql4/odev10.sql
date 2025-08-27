91efc96a1d2fc230b8fd7fedda0dabea4b9907be
ofarukbulut <96415105+ofarukbulut@users.noreply.github.com>
2025-08-27 16:13:47 +0300

    sql 10 ödev eklendi

---
+++ src/main/java/bulut/com/week6/sql4/odev10.sql
@@ -0,0 +1,20 @@
+--1. SORGU
+SELECT
+    ct.city, cn.country
+FROM
+    city as ct
+        LEFT JOIN country as cn ON cn.country_id = ct.country_id
+
+--2. SORGU
+SELECT
+    p.payment_id, c.first_name, c.last_name
+FROM
+    customer as c
+        RIGHT JOIN payment as p ON p.customer_id = c.customer_id
+
+--3. SORGU
+SELECT
+    r.rental_id, c.first_name, c.last_name
+FROM
+    customer as c
+        FULL JOIN rental as r ON r.customer_id = c.customer_id
